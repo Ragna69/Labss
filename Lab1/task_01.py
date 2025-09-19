dd6ab4267518f0139601aefdba0722e2f47302a7
Макс <svetachapa1963@gmail.com>
2025-09-19 11:00:31 +0300

    Задание про ФИО

--- Lab1/task_01.py
+++ Lab1/task_01.py
@@ -1,1 +1,7 @@
-print(1)
+a = input('Введите Фамилию:\n')
+b = input('Введите Имя:\n')
+c = input('Введите Отчество:\n')
+if not a or not b or not c:
+    print('Все поля должны быть заполнены')
+result = a + ' ' + b[0] + '.' + c[0] + '.'
+print(result)
