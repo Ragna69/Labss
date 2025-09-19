eeaeff3483cfd78a77f51c0c3e57485afe0f9e1b
Макс <svetachapa1963@gmail.com>
2025-09-19 11:04:12 +0300

    Удаление гласных букв

---
+++ Lab1/task_02.py
@@ -0,0 +1,13 @@
+
+s = input('Введите строку:')
+if not s:
+    print('Пустая строка')
+else:
+        g = "aeiouAEIOUуеыаоэяию"
+        result = ""
+        for char in s:
+            if char not in g:
+                result += char
+        print("Результат:", result)
+
+
