20b8a5851a144cba88713fc86dbfa19c304e3065
Макс <svetachapa1963@gmail.com>
2025-09-20 16:18:35 +0300

    IP адрес

--- Lab1/task_09.py
+++ Lab1/task_09.py
@@ -10,18 +10,16 @@ else:
         if ip_parts[0] < 0 or ip_parts[0] > 255:
             print('Неверное число в первой части IP:')
         else:
-            a = ip_parts[0]
             if ip_parts[1] < 0 or ip_parts[1] > 255:
                 print('Неверное число во второй части IP:')
             else:
-                b = ip_parts[1]
                 if ip_parts[2] < 0 or ip_parts[2] > 255:
                     print('Неверное число в третьей части IP:')
                 else:
-                    c = ip_parts[2]
                     if ip_parts[3] < 0 or ip_parts[3] > 255:
                         print('Неверное число в четвертой части IP:')
                     else:
-                        d = ip_parts[3]
                         ip_end = '.'.join(str(part) for part in ip_parts)
                         print('Ваш IP', ip_end, 'корректный')
+
+# знаю, что можно было короче, но я хотел, чтобы писало в какой именно части ip ошибка
