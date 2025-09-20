659c7ba39bbb2e729db6c71e5022020dbf7ead98
Макс <svetachapa1963@gmail.com>
2025-09-20 16:31:31 +0300

    IP адрес

--- Lab1/task_09.py
+++ Lab1/task_09.py
@@ -1,25 +1,15 @@
 ip = input('Введите IP для проверки на корректность:\n')
-ip = ip.split('.')
-ip_parts = [int(part) for part in ip]
+ipp = ip.split('.')
 if not ip:
     print('Строка пустая!')
 else:
-    if len(ip) != 4:
+    if len(ipp) != 4:
         print('Неверный формат IP адреса!')
     else:
-        if ip_parts[0] < 0 or ip_parts[0] > 255:
-            print('Неверное число в первой части IP:')
+        if not ipp[0].isdigit() or 0 > int(ipp[0]) > 255 or not\
+             ipp[1].isdigit() or 0 > int(ipp[1]) > 255 or not\
+             ipp[2].isdigit() or 0 > int(ipp[2]) > 255 or not \
+             ipp[3].isdigit() or 0 > int(ipp[3]) > 255:
+            print('Ваш IP', ip, 'некорректный')
         else:
-            if ip_parts[1] < 0 or ip_parts[1] > 255:
-                print('Неверное число во второй части IP:')
-            else:
-                if ip_parts[2] < 0 or ip_parts[2] > 255:
-                    print('Неверное число в третьей части IP:')
-                else:
-                    if ip_parts[3] < 0 or ip_parts[3] > 255:
-                        print('Неверное число в четвертой части IP:')
-                    else:
-                        ip_end = '.'.join(str(part) for part in ip_parts)
-                        print('Ваш IP', ip_end, 'корректный')
-
-# знаю, что можно было короче, но я хотел, чтобы писало в какой именно части ip ошибка
+            print('Ваш IP', ip, 'корректный')
